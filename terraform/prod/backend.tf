4b83e0df6d1ab9e019cf5b3a8469cdb8482ce204
zenitugo <ugochiukaegbu19@gmail.com>
2026-09-10 06:10:08 +0100

    changed bucket name

--- terraform/prod/backend.tf
+++ terraform/prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" { 
-        bucket = "orderly-k8s-buck"
+        bucket = "ugochi-orderly"
         key     = "orderly/terraform.tfstate"
         dynamodb_table = "orderly-platform-lock"
         region = "eu-central-1"
