3c60642cb646d8a4eb7d38e6f1838d5fcf2045c2
zenitugo <ugochiukaegbu19@gmail.com>
2026-09-17 06:39:37 +0100

    included policy attachment to cni_policy

--- terraform/modules/iam/node-group-role.tf
+++ terraform/modules/iam/node-group-role.tf
@@ -33,3 +33,8 @@ resource "aws_iam_role_policy_attachment" "ContainerRegistry" {
   policy_arn                      = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role                            = aws_iam_role.node_group.name
 }
+
+resource "aws_iam_role_policy_attachment" "CNI_Policy" {
+    policy_arn                    = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy" 
+    role                          = aws_iam_role.node_group.name
+}
